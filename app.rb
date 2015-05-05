6197ee1783c94d3711ef5de2b69aeb91514eb874
Courtney Phillips & peter <student@epicodus.com>
2015-05-05 16:09:12 -0700

    Save new patients and return them under corresponding doctors. All tests pass.

--- app.rb
+++ app.rb
@@ -32,3 +32,19 @@ get('/patients/:id') do
   @doctor = Doctor.find(params.fetch("id").to_i)
   erb(:patient)
 end
+
+get('/patient/:id/patient_form') do
+  @doctor = Doctor.find(params.fetch("id").to_i)
+  erb(:patient_form)
+end
+
+post('/add_patient') do
+  doctor_id = params.fetch('doctor_id').to_i
+  name = params.fetch("name")
+  birthdate = params.fetch("birthdate")
+  new_patient = Patient.new({:name => name, :birthdate => birthdate,  :doctor_id => doctor_id, :id => nil})
+  new_patient.save()
+  #@doctor = Doctor.find(doctor_id)
+  url = "/patients/"+ (doctor_id.to_s)
+  redirect(url)
+end
